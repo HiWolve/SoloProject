3e4b1139481a657e701fdb9e0cf857fea5506f70
Kreston <kreston.cm@gmail.com>
2021-12-07 06:31:35 -0600

    Login route complete

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,22 +1,14 @@
 // backend/routes/api/index.js
 const router = require('express').Router();
-const { restoreUser } = require('../../utils/auth.js');
-const { requireAuth } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
+router.use('/session', sessionRouter);
 
+router.use('/users', usersRouter);
 
-router.post('/test', function(req, res) {
+router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
-
-// GET /api/set-token-cookie
-const asyncHandler = require('express-async-handler');
-const { setTokenCookie } = require('../../utils/auth.js');
-const { User } = require('../../db/models');
-
-
-
-
-
 
 module.exports = router;
